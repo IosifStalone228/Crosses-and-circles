ca11c4606cc6e82e59daaec4a06ec65ce6a9694e
IosifStalone228 <115740050+IosifStalone228@users.noreply.github.com>
2022-11-14 19:27:58 +0300

    Add files via upload

---
+++ Crosses_and_circles.py
@@ -0,0 +1,55 @@
+board = list(range(1,10))
+
+def draw_board(board):
+   for i in range(3):
+      print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+      print("-" * 13)
+
+def player_input(player_token):
+    yes = False
+    while not yes:
+        player_answer = input("Куда поставим " + player_token + "? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print("Некорректный ввод. Вы уверены, что ввели число?")
+            continue
+        if player_answer >= 1 and player_answer <= 9:
+            if (str(board[player_answer - 1]) not in "XO"):
+                board[player_answer - 1] = player_token
+                valid = True
+            else:
+                print("Эта клетка уже занята!")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9.")
+
+def check_win(board):
+   win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
+   for coord in win_coord:
+       if board[coord[0]] == board[coord[1]] == board[coord[2]]:
+          return board[coord[0]]
+   return False
+
+def main(board):
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+           player_input("X")
+        else:
+           player_input("O")
+        counter += 1
+        if counter > 4:
+           symb = check_win(board)
+           if symb:
+              print(symb, "выиграл!")
+              win = True
+              break
+        if counter == 9:
+            print("Ничья!")
+            break
+    draw_board(board)
+main(board)
+
+input("Нажмите Enter для выхода!")
